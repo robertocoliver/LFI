8456141466f7304c48c1372b4b0a24f46644971e
Roberto Oliveira <robertocoliver@protonmail.com>
2023-04-20 05:22:07 -0300

    Update LFI.py

--- lab/LFI.py
+++ lab/LFI.py
@@ -1,22 +1,15 @@
-import os
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route('/')
-def listar_arquivos():
-    # Obtém o nome do diretório a ser listado a partir da query string
-    diretorio = request.args.get('params', default='.', type=str)
-
-    # Obtém o caminho absoluto do diretório a ser listado
-    caminho_absoluto = os.path.abspath(diretorio)
-
-    # Verifica se o caminho absoluto pertence ao diretório raiz do projeto
-    pasta_raiz = os.path.abspath(os.path.dirname(__file__))
-    if not caminho_absoluto.startswith(pasta_raiz):
-        return 'Acesso negado!'
-    else:
-	      return 'Acesso permitido!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Página Vulnerável</title>
+  </head>
+  <body>
+    <h1>Bem-vindo à Página Vulnerável!</h1>
+    <?php
+      // Recebe um parâmetro 'page' do usuário e inclui o arquivo com base nesse parâmetro
+      $page = $_GET['page'];
+      include($page . '.php');
+    ?>
+  </body>
+</html>
